dbc0487c4810d38ec9598924408f0bac81b538da
guoqing <350162775@qq.com>
2022-09-22 15:41:46 +0800

    修复启动报错

--- zone/src/main/java/com/texasthree/zone/club/MemberData.java
+++ zone/src/main/java/com/texasthree/zone/club/MemberData.java
@@ -1,12 +1,10 @@
 package com.texasthree.zone.club;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
 /**
  * @author: neo
  * @create: 2022-09-22 13:24
  */
-@Entity
+//@Entity
+//@Table(name = "member_data")
 public class MemberData {
 }
